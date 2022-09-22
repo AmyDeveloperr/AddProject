645344165c5f5bbcb9f5a381387baaf2b3157668
AmyDeveloperr <amar321dombe@gmail.com>
2022-09-23 03:54:35 +0530

    Task 8 completed

--- Tasks/Task8.js
+++ Tasks/Task8.js
@@ -1,9 +1,17 @@
 var form = document.getElementById("addForm");
+console.log(form);
 var itemList = document.getElementById("items");
 var filter = document.getElementById("filter");
 
+//Adding description
+//var textSelect = document.querySelectorAll("")
+var text = document.createElement("input");
+    text.setAttribute("type", "text");
+    text.className = "form-control mr-2";
+    text.id = "item2";
+    form.insertBefore(text, form.children[1]);
 
-
+    
 form.addEventListener("submit", addItem);
 
 function addItem(e) {
@@ -16,9 +24,15 @@ function addItem(e) {
     // li.appendChild(document.createTextNode(newItem));
     // itemList.insertBefore(li, itemList.children[0]);
     
-    li.appendChild(document.createTextNode(newItem));
+    var text1 = document.createTextNode(newItem);
+    text1.id = "text1";
+    li.appendChild(text1);
 
     itemList.appendChild(li);
+
+    var itemDes = document.getElementById("item2").value;
+    li.appendChild(document.createTextNode(" " + itemDes));
+
 
     var delButton = document.createElement("button");
 
@@ -40,13 +54,6 @@ for (var i = 0; i < li.length; i++) {
 
 }
 
-
-
-
-
-
-
-
 itemList.addEventListener("click", removeItem);
 
 function removeItem(e) {
@@ -62,15 +69,14 @@ filter.addEventListener("keyup", filterItems);
 
 function filterItems(e) {
     var text = e.target.value.toLowerCase();
-    console.log(text);
     var items = itemList.getElementsByTagName("li");
 
     Array.from(items).forEach(function(item){
 
         var itemName = item.firstChild.textContent;
+        var itemName2 = item.childNodes[1].textContent;
         console.log(itemName);
-
-            if (itemName.toLowerCase().indexOf(text) != -1) {
+            if (itemName.toLowerCase().indexOf(text) != -1 || itemName2.toLowerCase().indexOf(text) != -1) {
                 item.style.display = "block";
             } else {
                 item.style.display = "none";
