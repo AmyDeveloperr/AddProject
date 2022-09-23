8f588809a2287f5ce013e234f1f6c6ec8aaacea2
AmyDeveloperr <amar321dombe@gmail.com>
2022-09-23 21:42:07 +0530

    Appointment App Completed

--- Task10/Task10.js
+++ Task10/Task10.js
@@ -14,10 +14,10 @@ function onSubmit(e) {
         msg.innerHTML = "Please Enter All Fields";
         setTimeout(() => msg.remove(), 4000);
     } else {
-        const li = document.createElement("li");
-        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        // const li = document.createElement("li");
+        // li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
 
-        userList.appendChild(li);
+        // userList.appendChild(li);
 
         //storing user data in object  format
         const userDetails = {
@@ -25,7 +25,10 @@ function onSubmit(e) {
             Email: emailInput.value
         }
 
-       
+       //clearing fields
+
+       nameInput.value = "";
+       emailInput.value = ""; 
 
         //Representing objects as string->serialization
 
@@ -33,16 +36,52 @@ function onSubmit(e) {
 
         //storing all input data in local storage
         localStorage.setItem(userDetails.Email , seri);
-       
-        // localStorage.getItem("userDetails", deseri);
 
-        // localStorage.setItem("Email", seri);
-
-        //clearing fields
-        nameInput.value = "";
-        emailInput.value = "";
-
-        
-
+        showUsersOnScreen(userDetails);
     }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+Object.keys(localStorage).forEach((key) => {
+
+    const stringifiedDetails = localStorage.getItem(key);
+    const details = JSON.parse(stringifiedDetails);
+    showUsersOnScreen(details);
+})
+
+})
+
+function showUsersOnScreen(user) {
+    if (localStorage.getItem(user.Email) !== null) {
+        removeUserFromScreen(user.Email);
+    }
+const parentNode=document.getElementById("users");
+const childHTML=`<li id=${user.Email}> ${user.Name} : ${user.Email} 
+<button onclick=deleteUser("${user.Email}")> Delete User </button> 
+<button onclick=editDetails("${user.Email}","${user.Name}")>Edit Details </button>
+</li>`;
+parentNode.innerHTML = parentNode.innerHTML + childHTML;
+}
+
+//Edit user details
+
+function editDetails(email,name) {
+    document.getElementById("email").value = email;
+    document.getElementById("name").value = name;
+    deleteUser(email);
+}
+
+//Delete user
+
+function deleteUser(emailId) {
+    localStorage.removeItem(emailId);
+    removeUserFromScreen(emailId);
+}
+
+function removeUserFromScreen(emailId) {
+    const parentNode = document.getElementById('users');
+    const deleteChild = document.getElementById(emailId);
+        if (deleteChild) {
+    parentNode.removeChild(deleteChild);
+ }
 }
