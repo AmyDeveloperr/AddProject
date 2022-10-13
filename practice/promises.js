a97ef541af466a000ce93dd1f466c125a4642059
AmyDeveloperr <amar321dombe@gmail.com>
2022-10-13 23:38:22 +0530

    promises task completed

---
+++ practice/promises.js
@@ -0,0 +1,97 @@
+
+
+const arr = [
+    {cart: "add items into cart", createdAt: new Date().getSeconds()},
+    {cart: "checkout from cart", createdAt: new Date().getSeconds()}
+    
+]
+
+function cart() {
+    setTimeout(() => {
+
+        let output = '';
+        for (let i = 0; i < arr.length; i++) {
+           output += `<li>${arr[i].cart} - created ${new Date().getSeconds() - arr[i].createdAt} seconds ago </li>`;
+        }
+        document.body.innerHTML = output;
+    }, 1000)    
+    
+}
+
+function emptyCart(items) {
+
+    return new Promise ((res, rej) =>{
+
+        setTimeout(() => {
+
+            arr.push({...items, createdAt: new Date().getSeconds()});
+
+            const error = false;
+            if(!error) {
+                res();
+            } else {
+                rej("Error: something went wrong");
+            }
+
+            
+        },1000)
+    })   
+}
+
+function signOut(items) {
+
+    return new Promise ((res, rej) =>{
+
+        setTimeout(() => {
+
+            arr.push({...items, createdAt: new Date().getSeconds()});
+
+            const error = false;
+            if(!error) {
+                res();
+            } else {
+                rej("Error: something went wrong");
+            }
+
+            
+        },0)
+    })   
+
+}
+
+function deletePost() {
+    return new Promise ((res, rej) => {
+        
+        setTimeout(() => {
+            if (arr.length > 0) {
+                const lastElement = res(arr.pop());
+                res(lastElement);
+            } else {
+                rej("Array is empty now")
+            }
+            
+        },1000)
+        
+    })
+}
+
+
+emptyCart({cart: "Empty the cart"})
+.then(()=> {
+    signOut({cart:'sign out from cart'}).then(() => {
+     cart();
+    deletePost().then(() => {
+        cart();
+        deletePost().then(() => {
+            cart()
+            deletePost().then(() => {
+                cart();
+            deletePost().then(() => {
+                cart();
+            }).catch(err => console.log(err));
+        }).catch(err => console.log(err));
+        }).catch(err => console.log(err));
+    }).catch(err => console.log(err));
+}).catch(err => console.log(err));
+    }).catch(err => console.log(err));
+    
